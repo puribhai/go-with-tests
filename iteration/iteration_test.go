ecfc23af696c9ea5bfd78da31418cfa220dbd966
Anmol Puri <anmolpuri954@gmail.com>
2025-08-16 18:53:33 +0530

    test(iteration): add more test cases for Repeat function
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -8,6 +8,18 @@ func TestRepeat(t *testing.T) {
 		want := "aaaaa"
 		assertRepeat(t, got, want)
 	})
+
+	t.Run("when str is 'ab'", func(t *testing.T) {
+		got := Repeat("ab")
+		want := "ababababab"
+		assertRepeat(t, got, want)
+	})
+
+	t.Run("when str is empty", func(t *testing.T) {
+		got := Repeat("")
+		want := ""
+		assertRepeat(t, got, want)
+	})
 }
 func assertRepeat(t testing.TB, got, want string) {
 	t.Helper()
