660bf8ca7ce9be874cdafc0ffcc496a9633bc68c
Anmol Puri <anmolpuri954@gmail.com>
2025-08-16 19:22:40 +0530

    refactor(iteration): optimize Repeat function using strings.Builder
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,10 +1,12 @@
 package iteration
 
+import "strings"
+
 // Repeat returns a new string consisting of the input character repeated 5 times
 func Repeat(str string) string {
-	var ans string
+	var ans strings.Builder
 	for i := 0; i < 5; i++ {
-		ans += str
+		ans.WriteString(str)
 	}
-	return ans
+	return ans.String()
 }
