cac375bab2ca9c3c2f73d3ff0d22534ce13724c4
Anmol Puri <anmolpuri954@gmail.com>
2025-08-18 18:15:00 +0530

    test(arrays): add test for Sum function
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>

---
+++ arrays/arrays_test.go
@@ -0,0 +1,16 @@
+package arrays
+
+import "testing"
+
+func TestArrays(t *testing.T) {
+	t.Run("collection of five numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+}
