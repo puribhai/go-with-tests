3988fe341af7d0c6e20d7a1e56f156d100550cae
Anmol Puri <anmolpuri954@gmail.com>
2025-08-28 09:06:35 +0530

    feat(structs): add Rectangle struct and refactor tests
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,7 +4,9 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	t.Run("check perimeter for some width and height", func(t *testing.T) {
-		got := Perimeter(4.0, 5.2)
+
+		rectangle := Rectangle{4.0, 5.2}
+		got := Perimeter(rectangle)
 		want := 18.40
 
 		if got != want {
@@ -14,11 +16,12 @@ func TestPerimeter(t *testing.T) {
 }
 func TestArea(t *testing.T) {
 	t.Run("check area for some width and height", func(t *testing.T) {
-		got := Area(4.0, 5.2)
+		rectangle := Rectangle{4.0, 5.2}
+		got := Area(rectangle)
 		want := 20.8
 
 		if got != want {
-			t.Errorf("got %f want %f", got, want)
+			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
 }
