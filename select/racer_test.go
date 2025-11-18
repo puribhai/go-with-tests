4fd0587cd2cd20a01328a8cec7449ed9f3b67f6a
Anmol Puri <mail@anmolpuri.dev>
2025-11-18 18:13:12 +0530

    feat(select): add Racer and Tests

---
+++ select/racer_test.go
@@ -0,0 +1,31 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowUrl := slowServer.URL
+	fastUrl := fastServer.URL
+
+	got := Racer(slowUrl, fastUrl)
+	want := fastUrl
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
