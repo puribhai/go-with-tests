2c0dcc6892f4a7c492a0cc784517689f38ca8f9c
Anmol Puri <mail@anmolpuri.dev>
2026-03-21 02:26:28 +0530

    feat(reflection): refactor walk func to iterate over string fields

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,9 +1,16 @@
 package reflection
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
 }
